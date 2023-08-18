7e4ec7a056e74e0edf6ca0cd2e92e6e0e976bc8a
kyakivanfrank <kyakivanfrank@gmail.com>
2023-08-18 19:30:23 +0300

    added more in about

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { content } from "../services/Content";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { createElement } from "react";
@@ -7,6 +7,17 @@ const Navbar = () => {
   const { nav } = content;
   const [showMenu, setShowMenu] = useState(false);
   const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    if (showMenu) {
+      const timeoutId = setTimeout(() => {
+        setShowMenu(false);
+      }, 10000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [showMenu]);
+
 
   return (
     <div className="w-full flex justify-center">
@@ -20,11 +31,18 @@ const Navbar = () => {
         className={`fixed  z-[999] flex items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300 ${showMenu ? "bottom-10" : "bottom-[-100%]"
           }`}
       >
-        {nav.map((item, i) => <a key={i}
-          href={item.link} onClick={() => setActive(i)} className={`text-xl p-2.5 rounded-full sm:cursor-pointer  ${i === active && "bg-dark_primary text-white"} `}  >
-          {createElement(item.icon)}
-        </a>
-        )}
+        {nav.map((item, i) => (
+          <a
+            key={i}
+            href={item.link}
+            onClick={() => setActive(i)}
+            className={`text-xl p-2.5 rounded-full sm:cursor-pointer  ${
+              i === active && "bg-dark_primary text-white"
+            } `}
+          >
+            {createElement(item.icon)}
+          </a>
+        ))}
       </nav>
     </div>
   );
